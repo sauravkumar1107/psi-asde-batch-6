4d2490082e8e77131b550da029078648e0be756a
sauravkumar1107 <46196100+sauravkumar1107@users.noreply.github.com>
2021-06-21 16:52:11 +0530

    Add files via upload

---
+++ projectadd/src/test/java/com/sapient/AppTest.java
@@ -0,0 +1,28 @@
+package com.sapient;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    public void test1() {
+	App obj = new App();
+	int c = obj.add(10, 20);
+	assertEquals(c,30);
+	}
+}
